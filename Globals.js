7a04380de2f66993d15ffff36670a98cd570e0f1
Daniel Margulis <damargulis@wustl.edu>
2018-02-07 23:00:50 -0600

    .

--- Globals.js
+++ Globals.js
@@ -1,5 +1,4 @@
 const DEV = require('./DEV.js');
 module.exports = {
     BASE_URL: 'http://' + DEV.SERVER_IP + ':' + DEV.SERVER_PORT,
-    WEB_BASE_URL: 'http://ec2-54-186-191-46.us-west-2.compute.amazonaws.com:3000',
 };
