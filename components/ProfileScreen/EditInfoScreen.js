9f03fb7e48bc1ac600daa2adc2be0114149975b9
Daniel Margulis <damargulis@wustl.edu>
2018-02-10 19:04:47 -0600

    folderize profile

---
+++ components/ProfileScreen/EditInfoScreen.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import {
+    Slider,
+    Picker,
+    TextInput,
+    Button,
+    View,
+    Text
+} from 'react-native';
+
+class EditInfoScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            edits: Object.assign({}, this.props.original),
+        };
+    }
+
+    setEdit(key, value) {
+        let edits = this.state.edits;
+        edits[key] = value;
+        this.setState({edits: edits});
+    }
+
+    render() {
+        return(
+            <View>
+                    <Text>Edit Info Form</Text>
+                    <Text>First Name:</Text>
+                    <TextInput 
+                        onChangeText={(val) => this.setEdit('firstName', val)}
+                    />
+                    <Text>Age: {this.state.edits.age}</Text>
+                    <Slider
+                        step={1}
+                        minimumValue={18}
+                        maximumValue={99}
+                        value={this.state.edits.age}
+                        onValueChange={(val) => this.setEdit('age', val)}
+                    />
+                    <Text>Occupation:</Text>
+                    <TextInput 
+                        value={this.state.edits.occupation} 
+                        onChangeText={(val) => this.setEdit('occupation', val)}
+                    />
+
+                    <Text>School:</Text>
+                    <TextInput 
+                        value={this.state.edits.school} 
+                        onChangeText={(val) => this.setEdit('school', val)}
+                    />
+                    <Text>Gender:</Text>
+                    <Picker
+                        selectedValue={this.state.edits.gender}
+                        onValueChange={(val)=>this.setEdit('gender', val)}
+                    >
+                        <Picker.Item label='Male' value='Male'/>
+                        <Picker.Item label='Female' value='Female'/>
+                    </Picker>
+                    <Text>Interested In:</Text>
+                    <Picker
+                        selectedValue={this.state.edits.interestsGender}
+                        onValueChange={
+                            (val)=>this.setEdit('interestsGender', val)
+                        }
+                    >
+                        <Picker.Item label='Male' value='Male'/>
+                        <Picker.Item label='Female' value='Female'/>
+                        <Picker.Item label='Any' value='Any'/>
+                    </Picker>
+                    <Text> {
+                        'Maximum Distance: '
+                        + this.state.edits.interestsDistance
+                        + ' miles'
+                    }</Text>
+                    <Slider
+                        step={1}
+                        minimumValue={1}
+                        maximumValue={300}
+                        value={this.state.edits.interestsDistance}
+                        onValueChange={
+                            (val) => this.setEdit('interestsDistance', val)
+                        }
+                    />
+                    <Text>Minimum Age: {this.state.edits.interestsAgeMin}</Text>
+                    <Slider
+                        step={1}
+                        minimumValue={18}
+                        maximumValue={this.state.edits.interestsAgeMax}
+                        value={this.state.edits.interestsAgeMin}
+                        onValueChange={
+                            (val) => this.setEdit('interestsAgeMin', val)
+                        }
+                    />
+                    <Text>Maximum Age: {this.state.edits.interestsAgeMax}</Text>
+                    <Slider
+                        step={1}
+                        minimumValue={this.state.edits.interestsAgeMin}
+                        maximumValue={99}
+                        value={this.state.edits.interestsAgeMax}
+                        onValueChange={
+                            (val) => this.setEdit('interestsAgeMax', val)
+                        }
+                    />
+                    <Button
+                        title="Cancel"
+                        onPress={() => this.props.closeModal()}
+                    />
+                    <Button
+                        title="Save"
+                        onPress={() => this.props.save(this.state.edits)}
+                    />
+            </View>
+        )
+    }
+}
+
+export default EditInfoScreen;
---
+++ components/ProfileScreen/EditPhotosScreen.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import {
+    Button,
+    Image,
+    View,
+    Text,
+    TouchableHighlight,
+    CameraRoll
+} from 'react-native';
+
+var ImagePicker = require('react-native-image-picker');
+const GLOBAL = require('./../../Globals');
+
+class EditPhotosScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            photos: this.props.photos.slice(),
+            newPhotos: [],
+            newData: null,
+            showNew: false,
+        }
+    }
+
+    addPhoto(){
+        let options = {
+            title: 'Upload Photo',
+        };
+        ImagePicker.showImagePicker(options, (response) => {
+            if(response.didCancel) {
+                console.log('User cancelled');
+            } else if(response.error) {
+                console.log('Image picker error', response.error);
+            } else {
+                let newPhotos = this.state.newPhotos;
+                let source = response.uri;
+                newPhotos.push({
+                    'uri': response.uri,
+                    'data': response.data,
+                    'path': response.path,
+                    'type': response.type
+                });
+                this.setState({
+                    newPhotos: newPhotos,
+                })
+            }
+        });
+    }
+
+    movePhoto(){
+    }
+
+    submitPhotos(){
+        let photo = this.state.newPhotos[0];
+        const data = new FormData();
+        data.append('photo', {
+            uri: photo.uri,
+            type: 'image/png',
+            name: 'testName',
+        });
+        fetch(GLOBAL.BASE_URL + '/user/' + this.props.userId + '/photos', {
+            method: 'POST',
+            body: data
+        }).then((response) => response.json())
+        .then((response) => {
+            let photoId = response.photoId
+            fetch(GLOBAL.BASE_URL + '/user/photo/' + photoId)
+            .then((response) => response.json())
+            .then((response) => {
+                this.setState({
+                    newData: response.data.data,
+                    showNew: true,
+                });
+            }).catch((error) => {
+                console.log(error);
+            });
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    renderPhoto(photoIndex) {
+        let photos = this.state.photos.concat(this.state.newPhotos);
+        let photo = photos[photoIndex];
+        if(photo) {
+            return (
+                <View style={{width: 130, height: 130}} key={photoIndex} >
+                    <Image 
+                        source={{ uri: photo.uri }}
+                        style={{width: 129, height: 129}}
+                        key={photoIndex}
+                    />
+                </View>
+            )
+        }else {
+            return (
+                <TouchableHighlight
+                    onPress={() => this.addPhoto()}
+                    key={photoIndex} 
+                    style={{width: 130, height: 130, backgroundColor: 'red'}}
+                >
+                    <Text style={{textAlign: 'center'}}>Add A Photo</Text>
+                </TouchableHighlight>
+            )
+        }
+    }
+
+    showNew() {
+        if(this.state.showNew) {
+            var b64encode = btoa(String.fromCharCode.apply(null, this.state.newData));
+            b64encode = 'data:imgae/jpeg;base64,' + b64encode;
+            return (
+                <View style={{width: 130, height: 130}} >
+                    <Image 
+                        source={{ uri: b64encode }}
+                        style={{width: 129, height: 129}}
+                    />
+                </View>
+            )
+        }
+    }
+
+    render() {
+        return(
+            <View>
+            <View>
+                <Text>Photo Editing Page</Text>
+                <View style={{
+                    width: 400,
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                }}>
+                    { this.showNew() }
+                    {
+                        Array(GLOBAL.MAX_PHOTOS).fill().map((_, index) => {
+                            return this.renderPhoto(index);
+                        })
+                    }
+                </View>
+                <Button
+                    title="Submit"
+                    onPress={() => this.submitPhotos()}
+                />
+                <Button
+                    title="Cancel"
+                    onPress={() => this.props.closeModal()}
+                />
+            </View>
+            </View>
+        )
+    }
+}
+
+export default EditPhotosScreen;
