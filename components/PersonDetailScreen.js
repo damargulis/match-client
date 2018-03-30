47b3d9f24ea8defeb069d5016db3056e0843fa6f
Daniel Margulis <damargulis@wustl.edu>
2018-03-29 19:31:52 -0500

    photo

--- components/PersonDetailScreen.js
+++ components/PersonDetailScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image, View, Text} from 'react-native';
 
 class PersonDetailScreen extends React.Component {
     render() {
         return (
             <View>
-                <Text>Picture Here</Text>
+                <Image
+                    style={{ height: 300, width: 300}}
+                    source={{uri: this.props.navigation.state.params.user.photoData}}
+                />
                 <Text>{this.props.navigation.state.params.user.firstName}</Text>
                 <Text>{this.props.navigation.state.params.user.age}</Text>
                 <Text>{this.props.navigation.state.params.user.school}</Text>
