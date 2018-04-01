262d4be686ec82f4a2dfb58c08d55588ff63f49c
Daniel Margulis <damargulis@wustl.edu>
2018-04-01 18:17:23 -0500

    small refactor

--- components/EventScreen.js
+++ components/EventScreen.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { 
-    Button, 
-    SectionList, 
-    StyleSheet, 
     Switch,
     Text, 
     View,
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import EventDetailsScreen from './EventScreen/EventDetailsScreen';
+import EventList from './EventScreen/EventList';
 
 const GLOBAL = require('./../Globals');
 
@@ -117,45 +115,8 @@ class MainScreen extends React.Component {
     }
 
     render() {
-        let sections = [];
         let events = this.state.allEvents ? 
             this.state.events : this.state.userEvents;
-        var prevDay = null;
-        var currData = [];
-
-        let currentDay = Date.now();
-        for(var i=0; i < events.length; i++){
-            let event = events[i];
-            if(event.date < currentDay) {
-                currData.push(event);
-            } else if(prevDay && prevDay.getDate() == event.date.getDate()
-                && prevDay.getMonth() == event.date.getMonth()
-                && prevDay.getYear() == event.date.getYear()
-            ){
-                currData.push(event);
-            }else{
-                if(prevDay){
-                    sections.push({
-                        title: prevDay.toLocaleDateString(),
-                        data: currData,
-                    });
-                } else {
-                    sections.push({
-                        title: 'Ongoing',
-                        data: currData,
-                    });
-                }
-                currData = [];
-                currData.push(event);
-                prevDay = event.date;
-            }
-        }
-        if(prevDay){
-            sections.push({
-                title: prevDay.toLocaleDateString(),
-                data: currData,
-            });
-        }
         return (
             <View 
                 style={{ 
@@ -179,36 +140,11 @@ class MainScreen extends React.Component {
                     <Text>All Events</Text>
                 </View>
                 <View style={{flex: 10}}>
-                    <SectionList
-                        sections={sections}
-                        renderItem={
-                            ({item}) => (
-                                <Button 
-                                    style={styles.item} 
-                                    title={item.type + ': ' + item.name} 
-                                    onPress={
-                                        () => this.props.navigation.navigate(
-                                            'Details', 
-                                            {
-                                                event: item,
-                                                refreshEvents: 
-                                                    this.getUserEvents
-                                                    .bind(this),
-                                            }
-                                        )
-                                    } 
-                                />
-                            )
-                        }
-                        renderSectionHeader={
-                            ({section}) => (
-                                <Text style={styles.sectionHeader}>
-                                    {section.title}
-                                </Text>
-                            )
-                        }
-                        keyExtractor={(item, index) => index}
+                    <EventList 
+                        events={events} 
                         onEndReached={this.onEndReached.bind(this)}
+                        navigation={this.props.navigation} 
+                        getUserEvents={this.getUserEvents.bind(this)}
                     />
                 </View>
             </View>
@@ -216,26 +152,6 @@ class MainScreen extends React.Component {
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 22,
-    },
-    sectionHeader: {
-        paddingTop: 2,
-        paddingLeft: 10,
-        paddingRight: 10,
-        paddingBottom: 2,
-        fontSize: 14,
-        fontWeight: 'bold',
-        backgroundColor: 'rgba(247,247,247,1.0)',
-    },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
-    },
-});
 
 const EventsScreen = StackNavigator({
     Home: {
---
+++ components/EventScreen/EventList.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { 
+    Button,
+    Text, 
+    SectionList,
+    StyleSheet, 
+} from 'react-native';
+
+class EventList extends React.Component {
+    render() {
+        let sections = [];
+        var prevDay = null;
+        var currData = [];
+
+        let events = this.props.events;
+        let currentDay = Date.now();
+        for(var i=0; i < events.length; i++){
+            let event = events[i];
+            if(event.date < currentDay) {
+                currData.push(event);
+            } else if(prevDay && prevDay.getDate() == event.date.getDate()
+                && prevDay.getMonth() == event.date.getMonth()
+                && prevDay.getYear() == event.date.getYear()
+            ){
+                currData.push(event);
+            }else{
+                if(prevDay){
+                    sections.push({
+                        title: prevDay.toLocaleDateString(),
+                        data: currData,
+                    });
+                } else {
+                    sections.push({
+                        title: 'Ongoing',
+                        data: currData,
+                    });
+                }
+                currData = [];
+                currData.push(event);
+                prevDay = event.date;
+            }
+        }
+        if(prevDay){
+            sections.push({
+                title: prevDay.toLocaleDateString(),
+                data: currData,
+            });
+        }
+
+        return (
+            <SectionList
+                sections={sections}
+                renderItem={
+                    ({item}) => (
+                        <Button 
+                            style={styles.item} 
+                            title={item.type + ': ' + item.name} 
+                            onPress={
+                                () => this.props.navigation.navigate(
+                                    'Details', 
+                                    {
+                                        event: item,
+                                        refreshEvents: this.props.getUserEvents,
+                                    }
+                                )
+                            } 
+                        />
+                    )
+                }
+                renderSectionHeader={
+                    ({section}) => (
+                        <Text style={styles.sectionHeader}>
+                            {section.title}
+                        </Text>
+                    )
+                }
+                keyExtractor={(item, index) => index}
+                onEndReached={this.props.onEndReached}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 22,
+    },
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 14,
+        fontWeight: 'bold',
+        backgroundColor: 'rgba(247,247,247,1.0)',
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+});
+
+export default EventList;
